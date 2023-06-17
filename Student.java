dd8869078989954ee1a8502d7a5c562aa02ac905
CoderVthakre <vaibhavthakre87@gmail.com>
2023-06-17 11:01:59 +0530

    Added

---
+++ Student.java
@@ -0,0 +1,15 @@
+package HashtableAssignment;
+
+public class Student {
+    int id;
+    String name, address, email;
+
+    public Student(int id, String name, String address, String email) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+}
+
+
